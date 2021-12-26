7ae8f32b8a85a20e801929a8d8461dc4e6c98c8d
axus001 <rameshkvaw@gmail.com>
2021-12-26 19:09:23 +0530

    Update Fizbuzz.java

--- Fizbuzz.java
+++ Fizbuzz.java
@@ -1,17 +1,14 @@
 package satsession;
 import java.util.*;
 public class Fizbuzz {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
 System.out.println("Please enter the max num limit for fizbuzz");
 Scanner scan = new Scanner(System.in);
 int num = scan.nextInt();
 fizzy(num);
 scan.close();
 	}
-	
-	public static void fizzy(int num) {
+public static void fizzy(int num) {
 for (int i = 1 ; i<=num;i++)
 {
 	if (i%3==0 && i%5==0)
@@ -30,9 +27,5 @@ for (int i = 1 ; i<=num;i++)
 	{
 		System.out.println(i);
 	}
-	}
-
-
-	}
-
 }
+}}
